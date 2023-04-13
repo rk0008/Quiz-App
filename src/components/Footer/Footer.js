532f8581b22056d965318086d4d8ecc808df41f9
Rohan keshari <2020chb1052@iitrpr.ac.in>
2023-04-14 00:24:14 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made By Rohan Keshari
+      {/* <a
+        // href="https://www.youtube.com/roadsidecoder"
+        // style={{ cursor: "pointer" }}
+      >
+        Roadside Coder
+      </a> */}
+    </div>
+  );
+};
+
+export default Footer;
